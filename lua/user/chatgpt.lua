80ada0be967b73792775b2330a43139aa1c3a01a
Pawel Markowski <pawmar@gmail.com>
2023-09-02 11:27:27 +0200

    basic chatgpt plugin installation

---
+++ lua/user/chatgpt.lua
@@ -0,0 +1,18 @@
+local M = {
+  "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+}
+
+function M.config()
+  require("chatgpt").setup()
+end
+
+return M
