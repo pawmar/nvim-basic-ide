f8972762e6e9f83b7c7325535a084ebb126560a0
Pawel Markowski <pawmar@gmail.com>
2023-10-02 22:26:32 +0200

    remove unnecessary setup call from chatgpt

--- lua/user/chatgpt.lua
+++ lua/user/chatgpt.lua
@@ -1,9 +1,6 @@
 local M = {
   "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
     dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
